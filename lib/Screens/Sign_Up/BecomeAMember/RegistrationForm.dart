78bcd818320367de0159eca0657bd035f2198a79
Pankaj <patilpankaj1772@gmail.com>
2023-02-02 23:05:59 +0530

    first commit

--- lib/Screens/Sign_Up/BecomeAMember/RegistrationForm.dart
+++ lib/Screens/Sign_Up/BecomeAMember/RegistrationForm.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 import 'package:tssia_replica/Controller/SigupController.dart';
 import 'package:tssia_replica/Generic/Common/CommonText.dart';
+import 'package:tssia_replica/Generic/Common/Common_Color.dart';
 
 class RegistrationForm extends StatefulWidget {
   const RegistrationForm({super.key});
@@ -46,176 +47,234 @@ class _RegistrationFormState extends State<RegistrationForm> {
                   ),
                 ),
                 MemberHeaderText('Company Name'),
-                TextFormField(
-                  controller: companyName,
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return 'Please Enter Company Name';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffe4e4e4),
+                Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(24),
+                  child: TextFormField(
+                    controller: companyName,
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please Enter Company Name';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      fillColor: PWhite,
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
                       ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                        ),
+                      ),
+                      hintText: 'Enter Your Company Name',
+                      hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 11.sp,
+                          fontFamily: 'Poppins'),
                     ),
-                    hintText: 'Enter Your Company Name',
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 11.sp,
-                        fontFamily: 'Poppins'),
                   ),
                 ),
                 MemberHeaderText('Username'),
-                TextFormField(
-                  controller: Username,
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return 'Please Enter Name of Username';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffe4e4e4),
+                Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(24),
+                  child: TextFormField(
+                    controller: Username,
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please Enter Name of Username';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      fillColor: PWhite,
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
                       ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                        borderSide: BorderSide(
+                          color: Color(0xffe4e4e4),
+                        ),
+                      ),
+                      hintText: 'Enter Your Username',
+                      hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 11.sp,
+                          fontFamily: 'Poppins'),
                     ),
-                    hintText: 'Enter Your Username',
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 11.sp,
-                        fontFamily: 'Poppins'),
                   ),
                 ),
                 MemberHeaderText('Email'),
-                TextFormField(
-                  controller: email,
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return 'Please Enter Email';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffe4e4e4),
+                Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(24),
+                  child: TextFormField(
+                    controller: email,
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please Enter Email';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      fillColor: PWhite,
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
                       ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                        borderSide: BorderSide(
+                          color: Color(0xffe4e4e4),
+                        ),
+                      ),
+                      hintText: 'Enter Your Email',
+                      hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 11.sp,
+                          fontFamily: 'Poppins'),
                     ),
-                    hintText: 'Enter Your Email',
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 11.sp,
-                        fontFamily: 'Poppins'),
                   ),
                 ),
                 MemberHeaderText('Mobile Number'),
-                TextFormField(
-                  controller: mobile,
-                  maxLength: 10,
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return 'Please Enter Mobile Number';
-                    } else if (val.length != 10) {
-                      return "Please Enter Mobile Number";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    counterText: '',
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffe4e4e4),
+                Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(24),
+                  child: TextFormField(
+                    controller: mobile,
+                    maxLength: 10,
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Please Enter Mobile Number';
+                      } else if (val.length != 10) {
+                        return "Please Enter Mobile Number";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      fillColor: PWhite,
+                      filled: true,
+                      counterText: '',
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
                       ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                        borderSide: BorderSide(
+                          color: Color(0xffe4e4e4),
+                        ),
+                      ),
+                      hintText: 'Enter Your Mobile Number',
+                      hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 11.sp,
+                          fontFamily: 'Poppins'),
                     ),
-                    hintText: 'Enter Your Mobile Number',
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 11.sp,
-                        fontFamily: 'Poppins'),
                   ),
                 ),
                 MemberHeaderText('Password'),
-                TextFormField(
-                  controller: password,
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return 'Enter Your Password';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffe4e4e4),
+                Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(24),
+                  child: TextFormField(
+                    controller: password,
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Enter Your Password';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      fillColor: PWhite,
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
                       ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                        borderSide: BorderSide(
+                          color: Color(0xffe4e4e4),
+                        ),
+                      ),
+                      hintText: 'Enter Your Password',
+                      hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 11.sp,
+                          fontFamily: 'Poppins'),
                     ),
-                    hintText: 'Enter Your Password',
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 11.sp,
-                        fontFamily: 'Poppins'),
                   ),
                 ),
                 MemberHeaderText('Confirm Password'),
-                TextFormField(
-                  controller: Confirmpassword,
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return 'Enter Your Confirm Password';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                      borderSide: BorderSide(
-                        color: Color(0xffe4e4e4),
+                Material(
+                  elevation: 3.0,
+                  borderRadius: BorderRadius.circular(24),
+                  child: TextFormField(
+                    controller: Confirmpassword,
+                    validator: (val) {
+                      if (val!.isEmpty) {
+                        return 'Enter Your Confirm Password';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.5),
+                          borderRadius: BorderRadius.circular(24)),
+                      fillColor: PWhite,
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
                       ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                        borderSide: BorderSide(
+                          color: Color(0xffe4e4e4),
+                        ),
+                      ),
+                      hintText: 'Enter Your Confirm Password',
+                      hintStyle: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 11.sp,
+                          fontFamily: 'Poppins'),
                     ),
-                    hintText: 'Enter Your Confirm Password',
-                    hintStyle: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 11.sp,
-                        fontFamily: 'Poppins'),
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(vertical: 1.2.h),
                   child: Row(
                     children: [
-                      MemberHeaderText('Become a'),
+                      MemberHeaderText('Register as'),
                       checkradio == 'Member'
                           ? IconButton(
                               onPressed: () {
@@ -322,8 +381,8 @@ class _RegistrationFormState extends State<RegistrationForm> {
       padding: EdgeInsets.symmetric(vertical: 1.8.h),
       child: CommonText(
         label: memberLabel,
-        fontw8: FontWeight.w500,
-        size: 12.sp,
+        fontw8: FontWeight.w400,
+        size: 11.sp,
       ),
     );
   }
